807a0e08ce13c48d286443e4ae399337209ca295
Alessio Bolognino <abolognino@pic-informatica.it>
2023-02-16 14:43:36 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/zsh
+#nvm use --delete-prefix v12.13.1
 yarn build && \
     aws s3 cp build/ s3://organicer/ --recursive --profile personal && \
     aws cloudfront create-invalidation --distribution-id E3O2QTIE7KB9ED --profile personal --paths '/*'
