9520b5e80785b2f68a91cc950dca0727695340d9
Alessio Bolognino <abolognino@pic-informatica.it>
2023-02-16 01:37:28 +0100

    ingrandite icone actionbar

--- src/components/OrgFile/components/ActionDrawer/components/ActionButton/index.js
+++ src/components/OrgFile/components/ActionDrawer/components/ActionButton/index.js
@@ -43,7 +43,7 @@ export default class ActionButton extends PureComponent {
       additionalClassName || '',
       {
         fas: !letter,
-        'fa-lg': !letter,
+        'fa-2xl': !letter,
         [`fa-${iconName}`]: !letter,
         'action-drawer__btn--with-sub-icon': !!subIconName,
         'btn--disabled': isDisabled,
