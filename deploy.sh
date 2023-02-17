260c0414ed4f804a3faf6a5de220e6a5ac331ba6
Alessio Bolognino <abolognino@pic-informatica.it>
2023-02-17 17:10:02 +0100

    no pager

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ set -exu
 #nvm use --delete-prefix v12.13.1
 yarn build && \
     aws s3 cp build/ s3://organicer/ --recursive --profile personal && \
-    aws cloudfront create-invalidation --distribution-id E3O2QTIE7KB9ED --profile personal --paths '/*'
+    aws cloudfront create-invalidation --distribution-id E3O2QTIE7KB9ED --profile personal --paths '/*' --no-cli-pager
 
