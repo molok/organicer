ad7e2b91868174278322a8351765adc6d046383a
Alessio Bolognino <abolognino@pic-informatica.it>
2023-02-18 22:02:02 +0100

    fix checkbox color

--- src/components/UI/Checkbox/index.js
+++ src/components/UI/Checkbox/index.js
@@ -1,41 +1,14 @@
 import React from 'react';
-import { Motion, spring } from 'react-motion';
-
-import { interpolateColors, rgbaObject, rgbaString } from '../../../lib/color';
 
 import './stylesheet.css';
 
-export default ({ state, onClick }) => {
-  const uncheckedColor = rgbaObject(255, 255, 255, 1);
-  const checkedColor = rgbaObject(238, 232, 213, 1);
-
-  const checkboxStyle = {
-    colorFactor: spring(
-      {
-        checked: 1,
-        partial: 1,
-        unchecked: 0,
-      }[state],
-      { stiffness: 300 }
-    ),
-  };
-
+export default ({state, onClick}) => {
   return (
-    <Motion style={checkboxStyle}>
-      {(style) => {
-        const backgroundColor = rgbaString(
-          interpolateColors(uncheckedColor, checkedColor, style.colorFactor)
-        );
-
-        return (
-          <div className="checkbox" onClick={onClick} style={{ backgroundColor }}>
-            <div className="checkbox__inner-container">
-              {state === 'checked' && <i className="fas fa-check" />}
-              {state === 'partial' && <i className="fas fa-minus" />}
-            </div>
-          </div>
-        );
-      }}
-    </Motion>
+    <div className="checkbox" onClick={onClick}>
+      <div className="checkbox__inner-container">
+        {state === 'checked' && <i className="fas fa-check"/>}
+        {state === 'partial' && <i className="fas fa-minus"/>}
+      </div>
+    </div>
   );
 };
